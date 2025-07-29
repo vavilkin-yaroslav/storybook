ea50b692a6ea53a6581dcfb8b675b18aa7a5558d
Yaroslav Vavilkin <y.vavilkin@tg.dunice.net>
2025-07-29 08:56:18 +0300

    refactor: улучшение кода
    улучшение кода

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,8 +7,8 @@ const config = {
   "addons": [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest",
+    // "@storybook/addon-a11y",
+    // "@storybook/addon-vitest",
     "@storybook/addon-themes"
   ],
   "framework": {
